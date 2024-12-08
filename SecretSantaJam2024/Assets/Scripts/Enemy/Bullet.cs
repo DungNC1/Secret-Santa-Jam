1eb7a963ee7da4a7dc29f42e1b6ac756de66390f
DungNC1 <142099972+DungNC1@users.noreply.github.com>
2024-12-08 12:24:13 +0700

    Shooting Enemy

---
+++ SecretSantaJam2024/Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifeTime;
+    private float lifeTimeCounter;
+
+    private Transform player;
+    private Vector2 target;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+
+        lifeTimeCounter = lifeTime;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        lifeTimeCounter -= Time.deltaTime;
+
+        if(lifeTimeCounter < 0 )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+            Destroy(gameObject);
+        }
+    }
+}
